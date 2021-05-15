a71fcea6091b46fecea328387afb7dfa3dd45fc0
ZEHRA <z.nuralkan@gmail.com>
2021-05-15 23:18:46 +0300

    MvcProjeKamp
    1/4

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+  public  class Content
+    {
+        [Key]
+        public int ContentID { get; set; }
+        [StringLength(1000)]
+        public string ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+      public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+        public int? WriterID { get; set; }
+        //? ile nullable type olur. 
+       public virtual Writer Writer { get; set; }
+             
+    }
+}
---
+++ MvcProjeKamp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcProjeKamp.MvcApplication" Language="C#" %>
